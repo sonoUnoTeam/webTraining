bc42d4970d4f05bf7b63b2fe6c1bf321f77a05e5
Gonza1290 <gonzacayo@gmail.com>
2026-06-30 22:33:29 +0000

    Clear Migraciones

--- webTraining/wsgi.py
+++ webTraining/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+#path a donde esta el manage.py de nuestro proyecto Django
+path = '/home/nbertaina/Training/webTraining'
+if path not in sys.path:
+    sys.path.append(path)
+
+#Para varias instancias corriendo
+os.environ['DJANGO_SETTINGS_MODULE'] =  'webTraining.settings'
+
+#Prevenir UnicodeEncodeError
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
+
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/webTraining')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webTraining.settings')
-
 application = get_wsgi_application()
